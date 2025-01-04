5860e0649168775d162f62432bcc9148c1239ce7
rishadK70 <rishadmhd70@gmail.com>
2025-01-04 16:24:30 +0530

    Add files via upload

---
+++ lib/services/api_services.dart
@@ -0,0 +1,20 @@
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+class NewsService{
+ Future<List<dynamic>> fetchNews(String category) async{
+   final response=await http.get(Uri.parse("https://gnews.io/api/v4/search?q=news&lang=ml&token=5c9a61efaa72f37e7216e88e9e048426"),
+   );
+   if (response.statusCode==200){
+     final data=json.decode(response.body);
+     print(response.body);
+
+     return data['articles'];
+   }
+   else{
+     throw Exception('Failed to load news');
+   }
+ }
+}
